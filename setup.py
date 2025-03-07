41efab27e215f60293552471fc29a85cdf95e161
Rudolf Kolbe <rkolbe96@gmail.com>
2025-03-07 11:22:22 +0100

    modernize project description

--- setup.py
+++ setup.py
@@ -1,45 +1,53 @@
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 import os
 import platform
+from setuptools.command.build_ext import build_ext
+from wheel.bdist_wheel import bdist_wheel
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+
+class bdist_wheel_abi3(bdist_wheel):
+    def get_tag(self):
+        python, abi, plat = super().get_tag()
+
+        if python.startswith("cp"):
+            # on CPython, our wheels are abi3 and compatible back to 3.7
+            return "cp37", "abi3", plat
+
+        return python, abi, plat
+
+
+def get_extra_compile_args():
+    system = platform.system()
+    if system == "Windows":
+        return ["/std:c++14"]
+    elif system == "Darwin":
+        return ["-std=c++11"]
+    else:
+        return ["-std=c++11", "-fms-extensions"]
+
 
 setup(
     name="texture2ddecoder",
-    description="a python wrapper for Perfare's Texture2DDecoder",
-    author="K0lb3",
-    version="1.0.4",
-    keywords=["astc", "atc", "pvrtc", "etc", "crunch", "dxt", "bcn", "eacr"],
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Topic :: Multimedia :: Graphics",
-    ],
-    url="https://github.com/K0lb3/texture2ddecoder",
-    download_url="https://github.com/K0lb3/texture2ddecoder/tarball/master",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
+    packages=["texture2ddecoder"],
+    package_data={"texture2ddecoder": ["*.py", "*.pyi", "py.typed"]},
     ext_modules=[
         Extension(
-            "texture2ddecoder",
+            "texture2ddecoder._texture2ddecoder",
             [
                 os.path.join(root, f)
-                for root, dirs, files in os.walk("src")
+                for root, _dirs, files in os.walk("src")
                 for f in files
                 if f[-3:] in ["cpp", "cxx"]
                 and f not in ["Texture2DDecoder.cpp", "AssemblyInfo.cpp"]
             ],
             language="c++",
             include_dirs=["src/Texture2DDecoder"],
-            extra_compile_args=["-std=c++11"]
-            + (["-fms-extensions"] if platform.system() == "Darwin" else []),
+            extra_compile_args=get_extra_compile_args(),
+            py_limited_api=True,
+            define_macros=[
+                ("Py_LIMITED_API", "0x03070000"),
+            ],
         )
     ],
+    cmdclass={"bdist_wheel": bdist_wheel_abi3},
 )
